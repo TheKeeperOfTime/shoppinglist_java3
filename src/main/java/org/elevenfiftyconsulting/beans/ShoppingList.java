8a3432f273edbdc41be96bc124e21d9adbfa786d
Travis <travis_falls@mac.com>
2016-11-29 14:49:55 -0500

    added schema info

--- src/main/java/org/elevenfiftyconsulting/beans/ShoppingList.java
+++ src/main/java/org/elevenfiftyconsulting/beans/ShoppingList.java
@@ -13,60 +13,68 @@ import javax.persistence.OneToOne;
 @Entity
 @Table(name = "Shopping_List")
 public class ShoppingList {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
-	
-	@OneToOne(optional=false)
-    @JoinColumn(name="user_id")
+
+	@OneToOne(optional = false)
+	@JoinColumn(name = "user_id")
 	private User userId;
-	
+
 	private String color;
-	
+
 	private String name;
-	
+
 	private OffsetDateTime createdUtc;
 	private OffsetDateTime modifiedUtc;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public User getUserId() {
 		return userId;
 	}
+
 	public void setUserId(User userId) {
 		this.userId = userId;
 	}
+
 	public String getColor() {
 		return color;
 	}
+
 	public void setColor(String color) {
 		this.color = color;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public OffsetDateTime getCreatedUtc() {
 		return createdUtc;
 	}
+
 	public void setCreatedUtc(OffsetDateTime createdUtc) {
 		this.createdUtc = createdUtc;
 	}
+
 	public OffsetDateTime getModifiedUtc() {
 		return modifiedUtc;
 	}
+
 	public void setModifiedUtc(OffsetDateTime modifiedUtc) {
 		this.modifiedUtc = modifiedUtc;
 	}
-	
-	
-	
-	
 
 }
