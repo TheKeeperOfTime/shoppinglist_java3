e9ab9be07f0a60546aa68dcd44bd1b9b4ee66d8b
ggurevich <gurevichgabriel@gmail.com>
2016-12-02 14:01:11 -0500

    commit to merge

--- src/main/java/org/elevenfiftyconsulting/controllers/ShoppingListItemController.java
+++ src/main/java/org/elevenfiftyconsulting/controllers/ShoppingListItemController.java
@@ -1,19 +1,14 @@
 package org.elevenfiftyconsulting.controllers;
-//
-//import static com.users.security.Role.ROLE_USER;
 
 import java.util.Date;
 
 import javax.validation.Valid;
 
-import org.elevenfiftyconsulting.beans.ShoppingList;
 import org.elevenfiftyconsulting.beans.ShoppingListItem;
-import org.elevenfiftyconsulting.beans.User;
-//import org.elevenfiftyconsulting.repositories.NoteRepository;
 import org.elevenfiftyconsulting.repositories.ShoppingListItemRepository;
 import org.elevenfiftyconsulting.repositories.ShoppingListRepository;
 import org.elevenfiftyconsulting.repositories.UserRepository;
-//import org.elevenfiftyconsulting.repositories.UserRepository;
+import org.elevenfiftyconsulting.security.Priority;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,14 +19,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//import com.users.beans.User;
-//import com.users.beans.UserRole;
-
 @Controller
-public class ShoppingListController {
-
-//	@Autowired
-//	private NoteRepository noteRepo;
+public class ShoppingListItemController {
 
 	@Autowired
 	private ShoppingListItemRepository shoppingListItemRepo;
@@ -41,53 +30,7 @@ public class ShoppingListController {
 
 	@Autowired
 	private UserRepository userRepo;
-
-	@RequestMapping("/")
-	public String index(Model model) {
-		return "redirect:/shoppinglists";
-	}
 	
-	@RequestMapping("/home")
-	public String home(Model model) {
-		return "index";
-	}
-	
-	@RequestMapping("/shoppinglists")
-	public String shoppingLists(Model model) {
-		model.addAttribute("shoppingLists", shoppingListRepo.findAll());
-		return "shoppingListItem/shoppingList";
-	}
-
-	@GetMapping("/shoppinglist/create")
-	public String shoppingListCreate(Model model) {
-		model.addAttribute("shoppingList", new ShoppingList());
-		return "shoppingListItem/shoppingListCreate";
-	}
-	
-	@PostMapping("/shoppinglist/create")
-	public String shoppingListCreate(@ModelAttribute ShoppingList shoppingList, BindingResult result, Model model) {
-
-//		if (result.hasErrors()) {
-//			model.addAttribute("shoppingList", shoppingList);
-//			return "shoppingListItem/shoppingListCreate";
-//		} else {
-//			shoppingListRepo.save(shoppingList);
-//			return "redirect:/shoppinglists";
-//		}
-		
-		User u = new User();
-		u.setFirstName("bob");
-		u.setLastName("bobby");
-		u.setActive(true);
-		userRepo.save(u);
-		shoppingList.setUser(u);
-		shoppingList.setCreatedUtc(new Date(System.currentTimeMillis()));
-		shoppingList.setModifiedUtc(new Date(System.currentTimeMillis()));
-		shoppingListRepo.save(shoppingList);
-		return "redirect:/shoppinglists";
-
-	}
-
 	@RequestMapping("/shoppinglist/{id}")
 	public String listShoppingListItems(@PathVariable int id, Model model) {
 		model.addAttribute("id", id);
@@ -108,13 +51,16 @@ public class ShoppingListController {
 	@PostMapping("/shoppinglistitem/create")
 	public String shoppingListItemCreate(@ModelAttribute @Valid ShoppingListItem shoppingListItem, BindingResult result, Model model) {
 
-//		shoppingListItem.setPriority(HIGH);
-//		shoppingListItem.getPriority();
+		
+		
+		shoppingListItem.setPriority(Priority.HIGH);
+		shoppingListItem.getPriority();
 		shoppingListItem.setCreatedUtc(new Date(System.currentTimeMillis()));
 		shoppingListItem.setModifiedUtc(new Date(System.currentTimeMillis()));
 		shoppingListItemRepo.save(shoppingListItem);
 		return "redirect:/shoppinglist/{id}";
 
 	}
+	
 
 }
